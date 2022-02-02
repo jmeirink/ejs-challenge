91af2d56288a8d9202a15174e6ca2262e5f3cd5f
jmeirink <jesse@meirink.com>
2022-02-02 12:07:54 -0600

    Used EJS to console.log specific array objects from the compose.ejs page

--- app.js
+++ app.js
@@ -13,34 +13,46 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-  res.render("home", {startingContent: homeStartingContent});
+let posts = [];
+
+app.get("/", function(req, res) {
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts
+  });
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+    contactContent: contactContent
+  });
 });
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
+app.get("/about", function(req, res) {
+  res.render("about", {
+    aboutContent: aboutContent
+  });
 });
 
-app.get("/compose", function(req, res){
+app.get("/compose", function(req, res) {
   res.render("compose");
 });
 
-app.post("/compose", function(req, res){
-  console.log(req.body.postTitle);
+app.post("/compose", function(req, res) {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
 });
-
-
-
-
-
-
 
 
 
